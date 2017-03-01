f3f27b1b9b0f76287331c5d9f0de3a382479aa4c
roy <rohit31roy97coder@gmail.com>
2017-03-01 09:31:33 +0530

    Nothing much

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@ echo "Welcome to the KGP Protein Dna Docker!"
 # For running 
 echo "Now running the python script main.py for processing coordinates....."
 
-cd ./scripts
-./main.py test.pdb test.pdb
-cd ..
+#cd ./scripts
+#./main.py test.pdb test.pdb
+#cd ..
 
 echo "Finished processing the coordinates."
 
@@ -15,6 +15,7 @@ cd src
 make
 echo "Build complete! Now running the docker..."
 ./main
+make clean
 echo "Finished running the docker"
 
 echo "End of job."
