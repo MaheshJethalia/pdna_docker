cfd45808afa2fd7aaf1fd144f6a9b07257c13964
roy <rohit31roy97coder@gmail.com>
2017-03-15 01:18:11 +0530

    Include rho parameter in creatematrix()

--- src/libs/molecule.cpp
+++ src/libs/molecule.cpp
@@ -9,13 +9,19 @@ using namespace std;
 *	NOTE: size is extern declared in molecule.h and defined in ../main.c
 */
 
+/* Class Molecule Constructor: creates a 3d matrix as per the value of size set in main.cpp
+ * and intializes it with the value 0*/
+
 Molecule::Molecule() {
 	matrix.resize(size * size * size, 0);
 	center_index = 0;
     no_of_atoms = 0;
 }
 
-int Molecule::CreateMatrix(vector<float>& X, vector<float>& Y, vector<float>& Z) {
+/* Function to create a matrix of the macromolecule from the x, y and z xoordiantes 
+ * passed as floating vectors in the parameters*/
+
+int Molecule::CreateMatrix(vector<float>& X, vector<float>& Y, vector<float>& Z, int rho) {
 	vector<int> vx, vy, vz;		// integer form of matrices X, Y, Z
 	float xc, yc, zc;			// to hold index of geometric center of molecule
 	xc = yc = zc = 0.0;
@@ -47,11 +53,12 @@ int Molecule::CreateMatrix(vector<float>& X, vector<float>& Y, vector<float>& Z)
 	}
 
 	for(int i = 0; i < X.size(); i++) {		// Creating the basic matrix
-		matrix[index(vx[i], vy[i], vz[i])] = -10;	// -10 = value for core atom of protein
+		matrix[index(vx[i], vy[i], vz[i])] = rho;	// -10 = value for core atom of protein
 	}
 
     return 0;
 }
+
 
 int Molecule::CenterMatrix() {
 	return 0;
