886c3239faae4a0ed2f0879108a96b14a0c5d7d0
roy <rohit31roy97coder@gmail.com>
2017-05-25 17:07:59 +0530

    defining some basic classes

--- src/libs/config.h
+++ src/libs/config.h
@@ -1,4 +1,11 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+class RotationalAngle {
+    public:
+        int alpha, beta, gamma;
+        RotationalAngle();
+        RotationalAngle(int, int, int);
+};
+
 #endif
--- src/libs/molecule.h
+++ src/libs/molecule.h
@@ -1,5 +1,37 @@
 #ifndef MOLECULE_H
 #define MOLECULE_H
 
+#include "config.h"
+#include <string>
+#include <vector>
+
+enum biomol_type { PRO, DNA };
+
+class Coordinate {
+    public:
+        int x, y, z;
+
+        Coordinate();
+        Coordinate(int, int, int);
+        Coordinate Rotate(RotationalAngle&);
+};
+
+class Atom {
+    public:
+        Coordinate coord;
+        biomol_type type;
+        std::string name;
+
+        Atom();
+        Atom(Coordinate&, biomol_type, std::string&);
+};
+
+class Biomolecule {
+    public:
+        int noatoms;
+        biomol_type type;
+        Atom* atoms;
+        std::string PDB;
+};
 
 #endif
