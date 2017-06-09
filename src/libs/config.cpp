cad5e270d68770db1c9b937a0acc16edbf4610d7
roy <rohit31roy97coder@gmail.com>
2017-06-09 12:54:17 +0530

    Some major changes to config.h

--- src/libs/config.cpp
+++ src/libs/config.cpp
@@ -1,9 +1,26 @@
 #include "config.h"
 
+// Define macros for easy traversal of the container
+
+using namespace std;
+
 RotationalAngle::RotationalAngle() {
     alpha = beta = gamma = 0;
 }
 
 RotationalAngle::RotationalAngle( int a, int b, int g){
     alpha = a; beta = b; gamma = g;
+}
+
+
+Configuration::Configuration(){
+    noatoms = 0;
+}
+
+void Configuration::createConfiguration(Biomolecule* m, const RotationalAngle ang){
+    noatoms = m->noatoms; 
+    angle = ang;
+    for(vector<Atom>::iterator it = m->atoms.begin(); it!= m->atoms.end(); it++) { 
+    }
+     
 }
